476876ed1e99f0d47615989d442e4bffa7a5aba7
Daren Fankhauser <dfankhauser@Darens-Home-iMac.local>
2017-08-12 23:58:25 -0500

    Added code for League View to select league and enable Next button

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Daren Fankhauser on 8/12/17.
+//  Copyright © 2017 Daren Fankhauser. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
